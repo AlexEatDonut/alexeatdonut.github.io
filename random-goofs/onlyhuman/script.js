fd25a54947891c91a6084b7f439ac2fa8383d9ee
AlexEatDonut <alexeatdonut@gmail.com>
2024-04-30 14:31:05 +0200

    removed random-goofs - added links to onlyhuman netlify

--- random-goofs/onlyhuman/script.js
+++
@@ -1,7 +0,0 @@
-// let human = document.querySelector(".human");
-
-function fd(frameName) {
-  let human = document.querySelector(".human");
-  human.src = `./img/${frameName}.jpeg`;
-  // console.log("./img/${frameName}.jpeg");
-}
